4d7fa571864c63f9efa6921bae6a176b612f462e
fema <maria.felicita@empa.ch>
2024-05-20 13:08:30 +0200

    1605

--- simulations/SBR_implementation.py
+++ simulations/SBR_implementation.py
@@ -1,14 +1,79 @@
-import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from scipy.interpolate import RegularGridInterpolator
 from itertools import product
 
-from data_simulation import damping
+from data_simulation import filtered_database_full
 
 
 # extract CLT list from excel
 
 ES_RMS_table_full = pd.read_excel('Vloertrilling_prEN_SBR_EC5_17102023.xlsx', sheet_name = 'ES_RMS_90_full')
-ES_RMS_table = ES_RMS_table_full[ES_RMS_table_full['damping'] == damping]
+ES_RMS_table = ES_RMS_table_full[ES_RMS_table_full['damping'] == 0.025].copy()
 ES_RMS_table.set_index('f', inplace = True)
+ES_RMS_table.drop('damping', axis = 1, inplace = True)
 
-print(ES_RMS_table)
+
+x = ES_RMS_table.columns.astype(float).values
+y = ES_RMS_table.index.astype(float).values
+
+interpolator = RegularGridInterpolator((y, x), ES_RMS_table.values, method='linear')
+
+x_min, x_max = x.min(), x.max()
+y_min, y_max = y.min(), y.max()
+
+# Define a function to perform the interpolation
+def interpolate_value(row):
+    x_val = row['modal_mass']
+    y_val = row['natural_frequency']
+    # Clip values to be within the grid bounds
+    x_val_clipped = np.clip(x_val, x_min, x_max)
+    y_val_clipped = np.clip(y_val, y_min, y_max)
+    return interpolator((y_val_clipped, x_val_clipped))
+
+filtered_database_full_copy = filtered_database_full.copy()
+
+filtered_database_full_copy.loc[:, 'ES_RMS_value'] = filtered_database_full_copy.apply(interpolate_value, axis=1)
+
+ES_RMS_limits = {
+    'low_lim': [0.0, 0.1, 0.2, 0.8, 3.2, 12.8],
+    'high_lim': [0.1, 0.2, 0.8, 3.2, 12.8, 51.2]
+}
+
+response_classes = ['A', 'B', 'C', 'D', 'E', 'F']
+
+SBR_limits = pd.DataFrame(ES_RMS_limits, index = response_classes)
+
+response_class_list = []
+
+for index, row in filtered_database_full_copy.iterrows():
+    es_rms_value = row['ES_RMS_value']
+
+    for cls, limits in SBR_limits.iterrows():
+        low_lim = limits['low_lim']
+        high_lim = limits['high_lim']
+
+        if low_lim <= es_rms_value < high_lim:
+            response_class = cls
+            break
+
+    response_class_list.append(response_class)
+
+filtered_database_full_copy['response_class'] = response_class_list
+
+
+#colors = {'A': 'red', 'B': 'blue', 'C': 'green', 'D': 'orange', 'E': 'purple', 'F': 'black'}
+
+#plt.figure(figsize=(6,10))
+
+#for cls, color in colors.items():
+#    subset = filtered_database_full_copy[filtered_database_full_copy['response_class'] == cls]
+#    plt.scatter(subset['modal_mass'], subset['natural_frequency'], color = color, label = cls)
+
+#plt.xlabel('Modal Mass')
+#plt.ylabel('Natural Frequency')
+#plt.title('Scatter Plot of Modal Mass vs Natural Frequency')
+#plt.legend(title='Response Class')
+#plt.show()
+
